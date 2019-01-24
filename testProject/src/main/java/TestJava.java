6bbb0e72c43eb1548a5469b74db9c92c8de02bac
丁艳青 <1512650658@qq.com>
2019-01-24 21:24:32 +0800

    testGit

---
+++ testProject/src/main/java/TestJava.java
@@ -0,0 +1,11 @@
+/**
+ * @author 丁艳青
+ * @site www.dyq.com
+ * @company xxx
+ * @create 2019-01-24 21:21
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("gitzz");
+    }
+}

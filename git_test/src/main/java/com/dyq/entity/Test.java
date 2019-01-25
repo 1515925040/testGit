4cc7b35bd860f8d503ea1000c73a8e227135756a
dyq <992504740@qq.com>
2019-01-25 08:48:23 +0800

    test.java

---
+++ git_test/src/main/java/com/dyq/entity/Test.java
@@ -0,0 +1,13 @@
+package com.dyq.entity;
+
+/**
+ * @author 丁艳青
+ * @site www.dyq.com
+ * @company xxx
+ * @create 2019-01-25 8:47
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("zzzzzzz");
+    }
+}
